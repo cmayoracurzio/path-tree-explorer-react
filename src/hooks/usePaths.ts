487681963ff91828dff5fa4edda67850353be805
Carlos Mayora Curzio <carlos.mayora.curzio@gmail.com>
2023-10-11 17:08:51 +0200

    Simplify mock fetch function

--- src/hooks/usePaths.ts
+++ src/hooks/usePaths.ts
@@ -1,27 +1,6 @@
 import { useState, useEffect } from "react";
 
 /**
- * Mock fetch function that mimics fetching paths with a delay.
- *
- * @returns {Promise<string[]>} A promise that resolves to an array of paths.
- */
-const fetchPaths = async (): Promise<string[]> => {
-  const newPaths = await new Promise<string[]>((resolve) =>
-    setTimeout(() => {
-      resolve([
-        "/",
-        "/hello/example",
-        "hello/cheddar",
-        "/elephant/animal/zoo",
-        "elephant/plants",
-        "elephant/animal/building",
-      ]);
-    }, 1000)
-  );
-  return newPaths;
-};
-
-/**
  * `usePaths` - A custom hook that fetches and provides path data, as well as loading and error states.
  *
  * This hook fetches paths data, handles possible errors during fetching, and provides a way to track
@@ -47,7 +26,13 @@ const usePaths = (): {
 
     const loadPaths = async () => {
       try {
-        const newPaths = await fetchPaths();
+        // Mock fetch function that mimics fetching paths with a delay
+        const newPaths = await new Promise<string[]>((resolve) =>
+          setTimeout(() => {
+            resolve(["/root/test", "/dev/null", "/root/something/somewhere"]);
+          }, 1000)
+        );
+
         if (isMounted) {
           setPaths(newPaths);
           setIsLoading(false);
